83314461919c457251c21b35320ebe649f587667
Joe Duhamel <jduhamel@gmail.com>
2013-06-20 13:29:02 -0500

    Updated init.el

--- personal/personal.el
+++ personal/personal.el
@@ -2,7 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 (setq prelude-guru nil)
-(load-theme 'dark-laptop)
+;(load-theme 'dark-laptop)
+
+(add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/"))
+
+(load-file (expand-file-name "~/.emacs.d/themes/dark-laptop.el"))
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/personal/python-mode.el-6.0.12"))
 
@@ -12,8 +16,23 @@
 (setq py-shell-name "/usr/local/share/python3/ipython3")
 
 
-
-(prelude-ensure-module-deps '(go-mode go-autocomplete xml-rpc jira gist pastebin))
+(defun toggle-fullscreen ()
+  "Toggle fullscreen"
+  (interactive)
+  (set-frame-parameter
+   nil 'fullscreen
+   (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
+(global-set-key [f11] 'toggle-fullscreen)
+(prelude-ensure-module-deps '(go-mode
+                              go-autocomplete
+                              xml-rpc
+                              paredit
+                              color-theme
+                              deft
+                              jira
+                              gist
+                              pastebin
+                              ruby-compilation))
 
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
